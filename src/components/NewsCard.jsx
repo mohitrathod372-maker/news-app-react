0c503bd7b650fa4bbd0da6e1719a6927338eaa93
Mohit Rathod <mohitrathod372@gmail.com>
2026-05-24 00:03:57 +0530

    integrated news API and fatched news data

--- src/components/NewsCard.jsx
+++ src/components/NewsCard.jsx
@@ -1,7 +1,64 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import { fetchNews } from "../services/api.js";
 
 export default function NewsCard() {
+
+  const [news, setNews] = useState([]);
+
+  useEffect(() => {
+
+    const getNews = async () => {
+
+      const data = await fetchNews();
+
+      setNews(data);
+
+    };
+
+    getNews();
+
+  }, []);
+
   return (
-    <div>NewsCard</div>
-  )
+
+    <div className="min-h-screen bg-gradient-to-br from-slate-900 via-black to-slate-800 p-8">
+
+  <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+
+    {news.map((item, index) => (
+
+      <div
+        key={index}
+        className="bg-white/10 backdrop-blur-lg border border-white/10 rounded-3xl overflow-hidden shadow-2xl hover:scale-105 hover:shadow-cyan-500/30 transition-all duration-300"
+      >
+
+        <img
+          src={item.urlToImage}
+          alt=""
+          className="w-full h-[220px] object-cover"
+        />
+
+        <div className="p-5">
+
+          <h2 className="text-white text-2xl font-bold mb-3 line-clamp-2">
+            {item.title}
+          </h2>
+
+          <p className="text-gray-300 text-sm leading-6 line-clamp-3">
+            {item.description}
+          </p>
+
+          
+
+        </div>
+
+      </div>
+
+    ))}
+
+  </div>
+
+</div>
+  );
 }
+
---
+++ src/services/api.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const API_KEY = "13170c631efc4f6385cbfe4a12cf6f68";
+
+export const fetchNews = async (query = "india") => {
+
+  const response = await axios.get(
+    `https://newsapi.org/v2/everything?q=${query}&apiKey=${API_KEY}`
+  );
+
+  return response.data.articles;
+};
